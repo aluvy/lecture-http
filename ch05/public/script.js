d4e3eac063b7131650fe19437c7f3efd56ffaab6
mosong <mosong@the-51.com>
2025-09-01 14:17:53 +0900

    form GET

--- ch05/public/script.js
+++ ch05/public/script.js
@@ -1,3 +1,14 @@
+const insertTrackingPicxel = () => {
+  const img = document.createElement('img');
+  img.src = '/tracking-pixel.gif';
+  img.alt = 'Tracking Pixel';
+  img.style.width = '1px';
+  img.style.height = '1px';
+  img.style.display = 'none';
+  document.body.appendChild(img);
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('script.js');
+  insertTrackingPicxel(); // 광고픽셀
 });
--- ch05/server.js
+++ ch05/server.js
@@ -3,7 +3,48 @@ const path = require('path');
 
 const static = require('./shared/serve-static');
 
+const logRequest = (req) => {
+  const log = [
+    // 유저의 사용 시간을 알 수 있습니다.
+    `${new Date().toISOString()}`,
+
+    // 유저의 접속 지역을 알 수 있습니다.
+    `IP: ${req.socket.remoteAddress || req.connection.remoteAddress}`,
+
+    // 유저가 사용하는 단말기 정보를 알 수 있습니다.
+    `User-Agent: ${req.headers['user-agent']}`,
+
+    // 사용자가 어떤 페이지를 보는지 알 수 있습니다.
+    `Referer: ${req.headers['referer']}`,
+  ].join(', ');
+
+  console.log(log);
+};
+
+const getLogin = (req, res) => {
+  const { searchParams } = new URL(req.url, `http://${req.headers.host}`);
+  const email = searchParams.get('email');
+  const password = searchParams.get('password');
+
+  const authenticated = email === 'myemail' && password === 'mypassword';
+
+  if (!authenticated) {
+    res.statusCode = 401;
+    res.write('Unauthorized\n');
+    res.end();
+    return;
+  }
+
+  res.write('Success\n');
+  res.end();
+};
+
 const handler = (req, res) => {
+  const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+
+  if (pathname === '/tracking-pixel.gif') logRequest(req);
+  if (pathname === '/login') return getLogin(req, res);
+
   static(path.join(__dirname, 'public'))(req, res);
 };
 
