0b8939b2aef4cd57d0763ca0f829240592b1e56a
mosong <mosong@the-51.com>
2025-09-05 15:31:52 +0900

    superagent

---
+++ ch08/README.md
@@ -0,0 +1,46 @@
+# 03. AJAX
+
+🔗 https://github.com/jeonghwan-kim/lecture-http  
+🔗 https://jeonghwan-kim.github.io/2024/07/09/lecture-http-part3
+
+- 직접 만들 수 있는 HTTP 요청
+
+<hr>
+
+- 6장. AJAX 요청과 응답: fetch 함수로 AJAX 요청과 응답을 다루는 법에 대해
+- 7장. AJAX 진행율과 취소: AJAX 진행율을 계산하는 방법과 요청을 취소하는 방법에 대해
+- 8장. AJAX 라이브러리: fetch와 XHR 객체 기반의 주요 AJAX 라이브러리
+
+<br>
+
+# 8장. 라이브러리
+
+<br>
+
+## 8-1. SuperAgent
+
+- https://github.com/forwardemail/superagent
+- https://github.com/forwardemail/superagent?tab=readme-ov-file#plugins
+- XHR 기반. 2011년 출시. 콜백 스타일
+- HTTP 메소드별 전용 함수
+- Error 클래스를 확장한 오류 객체
+- 플러그인으로 확장
+
+<br>
+
+## 8-2. Axios
+
+- XHR 기반. 2014년 출시. 프라미스 기반
+- HTTP 메소드별 전용 함수
+- AxiosError
+- 인터셉터로 확장
+
+<br>
+
+## 8-3. Ky
+
+- fetch 기반. 2018년 출시. 타입스크립트
+
+<br>
+
+## 8-4. Wretch
---
+++ ch08/server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const path = require('path');
+const static = require('./shared/serve-static');
+
+const successApi = (req, res) => {
+  res.setHeader('Content-type', 'application/json');
+  res.write(JSON.stringify({ result: 'success' }));
+  res.end();
+};
+
+const failApi = (req, res) => {
+  res.statusCode = 400;
+  res.setHeader('Content-type', 'application/json');
+  res.write(JSON.stringify({ result: 'fail' }));
+  res.end();
+};
+
+const server = http.createServer((req, res) => {
+  const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+
+  if (pathname === '/api/success') return successApi(req, res);
+  if (pathname === '/api/fail') return failApi(req, res);
+
+  static(path.join(__dirname, 'public'))(req, res);
+});
+
+server.listen(3000, () => console.log('server is sunning ::3000'));
