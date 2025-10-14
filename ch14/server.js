35ceff4207fe9f765490ea71e35cc9318ebf96df
mosong <aluvy_@naver.com>
2025-10-14 16:23:20 +0900

    cors

--- ch14/server.js
+++ ch14/server.js
@@ -6,7 +6,7 @@ const handler = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // 출처명시: http://localhost:3000 도 사용 허용한다.
   res.setHeader('Access-Control-Allow-Headers', 'X-Foo'); // 커스텀 헤더 이름 명시
   res.setHeader('Access-Control-Allow-Methods', 'PUT'); // 사전요청 (PUT, PATCH, DELETE 메소드를 사용)
-  res.setHeader('Access-Control-Max-Age', '10');
+  res.setHeader('Access-Control-Max-Age', '10'); // 사전 요청은 캐시로 네트워크 비용을 아낄 수 있다.
 
   static(path.join(__dirname, 'public'))(req, res);
 };
