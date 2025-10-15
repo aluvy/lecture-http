317651a86cb55e1c7e8e660541b0b74d7df2686b
mosong <aluvy_@naver.com>
2025-10-15 16:00:20 +0900

    defer

--- ch16/public/script-big.js
+++ ch16/public/script-big.js
@@ -1,4 +1,6 @@
 console.log('script-big.js');
 
+window.foo = () => console.log('foo is excuted.');
+
 performance.mark('script-big-end');
 performance.measure('script-big excution time', 'script-big-start', 'script-big-end');
--- ch16/public/script-small.js
+++ ch16/public/script-small.js
@@ -1,4 +1,6 @@
 console.log('script-small.js');
 
+foo(); // Async 로 호출하면 에러 발생
+
 performance.mark('script-small-end');
 performance.measure('script-small excution time', 'script-small-start', 'script-small-end');
